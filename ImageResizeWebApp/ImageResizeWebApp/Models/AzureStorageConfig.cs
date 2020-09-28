dc6f7b5c050e8b8b1831d6dd61ab6c77fc30e983
davmhelm <dahelm@microsoft.com>
2020-09-28 17:01:54 -0500

    added support different storage accounts for uploads and thumbnails

--- ImageResizeWebApp/ImageResizeWebApp/Models/AzureStorageConfig.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Models/AzureStorageConfig.cs
@@ -3,7 +3,9 @@
     public class AzureStorageConfig
     {
         public string AccountName { get; set; }
+        public string ThumbnailAccountName { get; set; }
         public string AccountKey { get; set; }
+        public string ThumbnailAccountKey { get; set; }
         public string ImageContainer { get; set; }
         public string ThumbnailContainer { get; set; }
     }
